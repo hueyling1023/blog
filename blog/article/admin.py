04dd75fa3479c816319de7ad3d7bc37654ff375c
hueyling1023 <hueyling0327@gmail.com>
2018-12-17 15:29:37 +0800

    Chapter 7 finished

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -3,6 +3,10 @@ from article.models import Article, Comment
 
 class CommentModelAdmin(admin.ModelAdmin):
     list_display = ['article', 'content']
+    list_display_links = ['article']
+    list_filter = ['article', 'content']
+    search_fields = ['content']
+    list_editable = ['content']
  
     class Meta:
         model = Comment
