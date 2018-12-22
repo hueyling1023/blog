da12dc387169067138e5d63c4d92d57a51c6121a
hueyling1023 <hueyling0327@gmail.com>
2018-12-22 15:23:30 +0800

    221218

--- blog/article/views.py
+++ blog/article/views.py
@@ -10,3 +10,13 @@ def article(request):
         articles.update({article:Comment.objects.filter(article=article)})
     context = {'articles':articles}
     return render(request, 'article/article.html',context)
+
+def articleCreate(request):
+    '''
+    Create a new article instance
+        1. If method is GET, render an empty form
+        2. If method is POST, perform form validation and display error messages if the form is invalid
+        3. Save the form to the model and redirect the user to the article page
+    '''
+    # TODO: finish the code
+    return render(request, 'article/article.html')
