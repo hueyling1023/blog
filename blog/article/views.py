1e650ebd9e1e6f51813573cbc8a022861d34d941
hueyling1023 <hueyling0327@gmail.com>
2018-12-22 14:25:18 +0800

    12/22

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from article.models import Article,Comment
 
 def article(request):
     '''
     Render the article page
     '''
-    return render(request, 'article/article.html')
+    articles = {}
+    for article in Article.objects.all():
+        articles.update({article:Comment.objects.filter(article=article)})
+    context = {'articles':articles}
+    return render(request, 'article/article.html',context)
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'article',
     'main',
+    'article',
 ]
 
 MIDDLEWARE = [
